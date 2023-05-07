c4dc9835ca84550e6e564a9f98053975f7385356
topopelon <jlaguna98@gmail.com>
2023-05-07 14:02:48 +0100

    BERT

--- app/indexer.py
+++ app/indexer.py
@@ -1,9 +1,7 @@
 # Class to index .vec embeddings files (fasttext), apply PCA (sklearn) and store the vectors in elasticsearch
 
-from sklearn.decomposition import PCA
-from gensim.models import KeyedVectors
 from elasticsearch import Elasticsearch
-import time
+from sentence_transformers import SentenceTransformer
 
 
 class News:
@@ -14,121 +12,92 @@ class News:
 
 
 class Indexer:
-    def __init__(self, es: Elasticsearch, index, embedding_file, pca_n_components=50, pca_whiten=True):
+    def __init__(self, es, index, model_name):
         self.es = es
         self.index = index
-        self.embedding_file = embedding_file
-        self.pca_n_components = pca_n_components
-        self.pca_whiten = pca_whiten
-        self.model = None
-
-    def load_model_to_memory(self, apply_pca=False):
-        # Load trained model in memory
-        print("Loading model...")
-        self.model = KeyedVectors.load_word2vec_format(
-            self.embedding_file, binary=False)
-        print("model loaded.")
-
-        # Apply PCA
-        if apply_pca:
-            print("Applying PCA...")
-            pca = PCA(n_components=self.pca_n_components,
-                      whiten=self.pca_whiten)
-            self.model.vectors = pca.fit_transform(self.model.vectors)
-            print("PCA applied.")
+        self.model = SentenceTransformer(model_name)
 
     def create_index(self):
         # Create index
         print("Creating index...")
-
         settings = {
             "number_of_shards": 1,
-            "number_of_replicas": 0
+            "number_of_replicas": 0,
         }
         mappings = {
             "properties": {
-                "title": {
-                    "type": "text"
-                },
+                'title': {'type': 'text'},
                 "embeddings": {
                     "type": "dense_vector",
-                    "dims": self.model.vectors.shape[1]
+                    "dims": self.model.get_sentence_embedding_dimension()
                 }
             }
         }
         self.es.indices.create(
             index=self.index, settings=settings, mappings=mappings)
-
         print("Index created.")
 
-    def index_embeddings(self, news: list[News]):
+    def index_news(self, news: list[News]):
         # Index embeddings
-        print("Indexing embeddings...")
+        print("Indexing news...")
         for news in news:
-            # Average embeddings
-            words = []
-            title_lowercase = news.title.lower()
-            for word in title_lowercase.split():
-                if word in self.model:
-                    words.append(word)
-            embeddings = self.model[words].mean(
-                axis=0).tolist()
             doc = {
                 "title": news.title,
-                "embeddings": embeddings
+                "embeddings": self.model.encode(news.title)
             }
             self.es.index(index=self.index, body=doc)
+        print("News indexed.")
 
-        print("Embeddings indexed.")
-
-    def search(self, title):
-        # Search
-        print("Searching...")
-        # Make query with embeddings
+    def search(self, text):
+        # Encode query using model
+        query_embedding = self.model.encode(text)
+        # Combine title text search with dense vector search using cosine similarity
         query = {
-            "query": {
-                "script_score": {
-                    "query": {
-                        "match_all": {}
-                    },
-                    "script": {
-                        "source": "cosineSimilarity(params.query_vector, 'embeddings') + 1.0",
-                        "params": {
-                            "query_vector": self.model[title.lower().split()].mean(
-                                axis=0).tolist()
-                        }
+            "script_score": {
+                "query": {
+                    "match_all": {}
+                },
+                "script": {
+                    "source": "cosineSimilarity(params.query_vector, 'embeddings') + 1.0",
+                    "params": {
+                        "query_vector": query_embedding.tolist()
                     }
                 }
             }
         }
-        res = self.es.search(index=self.index, body=query)
-        print("Search done.")
-        return res
+
+        res = self.es.search(index=self.index, body={
+                             "size": 10, "query": query})
+
+        # Interpret results with highest score first
+        for hit in res["hits"]["hits"]:
+            print(hit["_score"], hit["_source"]["title"])
 
 
 if __name__ == "__main__":
-    es = Elasticsearch(hosts=["http://elasticsearch:9200"])
+    es = Elasticsearch(hosts=["http://elasticsearch:9200"], timeout=60)
     # Check if connection is ok
     if es.ping():
         print("Connected to elasticsearch")
     else:
         print("Could not connect to elasticsearch")
-    indexer = Indexer(es, "test", "data/corpus.vec")
+    indexer = Indexer(es, "test", "recobo/agriculture-bert-uncased")
 
+    if indexer.es.indices.exists(index="test"):
+        indexer.es.indices.delete(index="test")
+    indexer.create_index()
+    # Create some agriculture news
+    news = [
+        News("Machines are used in agriculture to plant, cultivate and harvest crops.", "", ""),
+        News("Agriculture is the science and art of cultivating plants and livestock.", "", ""),
+        News("Nuts are a rich source of energy and nutrients.", "", ""),
+        News("The main agricultural products can be broadly grouped into foods, fibers, fuels and raw materials.", "", ""),
+        News("Walnuts and almonds are the most popular nuts.", "", ""),
+        News("Jute is a long, soft, shiny bast fiber that can be spun into coarse, strong threads.", "", ""),
+        News("Corn is a grain first domesticated by indigenous peoples in southern Mexico about 10,000 years ago.", "", ""),
+        News("Quinoa and oats are grains that are rich in protein.", "", ""),
+    ]
+    indexer.index_news(news)
     while True:
-        user_input = input("Load model to memory?:(y/n) ")
-        if user_input == "y":
-            user_input = input("Apply PCA algorithm?:(y/n) ")
-            indexer.load_model_to_memory(apply_pca=user_input == "y")
-        if indexer.es.indices.exists(index="test"):
-            indexer.es.indices.delete(index="test")
-        indexer.create_index()
-        news = [News("Fribin tira con fuerza de la locomotora agroindustrial", "", ""), News("Riego inteligente para exportar mejores cerezas", "", ""), News("El dulce más popular y viajero de Barbastro celebra su 120 aniversario", "", ""), News(
-            "Nuevos tiempos para el conejo en Hispania", "", ""), News("Aniñón sabe de cerezas", "", ""), News("Viñas de la Denominación de Origen Campo de Borja", "", "")]
-        indexer.index_embeddings(news)
-        time.sleep(5)
         user_input = input("Write a sentence to search: ")
-        res = indexer.search(user_input)
-        # Interpret results with highes score first
-        for hit in res["hits"]["hits"]:
-            print(hit["_score"], hit["_source"]["title"])
+        indexer.search(user_input)
